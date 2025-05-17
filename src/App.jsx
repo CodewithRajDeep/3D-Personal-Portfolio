357ed5c422b89d4133f8a514f625f9d5821dc5e0
Deep Raj <deepraj6897@gmail.com>
2025-05-17 14:45:14 +0530

    models and home screen ready

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import Hero from "./sections/Hero";
+import ShowcaseSection from "./sections/ShowcaseSection";
+
+const App = () => (
+  <>
+    <Hero />
+    <ShowcaseSection />
+  </>
+);
+
+export default App;
