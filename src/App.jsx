a1149bfb98cfecba0933117540a2d19f0af5c6c6
Deep Raj <deepraj6897@gmail.com>
2025-05-18 08:39:49 +0530

    changes in layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
+import NavBar from "./components/Navbar";
 import Navbar from "./components/Navbar";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
 
 const App = () => (
   <>
+   <NavBar/>
     <Hero/>
     <ShowcaseSection />
   </>
