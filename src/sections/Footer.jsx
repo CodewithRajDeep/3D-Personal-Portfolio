57af55aa83f534e1a4f9a811a77c8be6a3693ce0
Deep Raj <deepraj6897@gmail.com>
2025-05-21 10:01:51 +0530

    updated footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -7,13 +7,21 @@ const Footer = () => {
         <div className="flex flex-col justify-center">
           <p>Terms & Conditions</p>
         </div>
-        <div className="socials">
+
+        <div className="socials flex gap-4 justify-center">
           {socialImgs.map((socialImg, index) => (
-            <div key={index} className="icon">
-              <img src={socialImg.imgPath} alt="social icon" />
-            </div>
+            <a
+              key={index}
+              href={socialImg.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="icon transition-transform hover:scale-110"
+            >
+              <img src={socialImg.imgPath} alt={socialImg.name} className="w-6 h-6" />
+            </a>
           ))}
         </div>
+
         <div className="flex flex-col justify-center">
           <p className="text-center md:text-end">
             © {new Date().getFullYear()} Deep Raj. All rights reserved.
